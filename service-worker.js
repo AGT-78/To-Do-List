fb623d37f110b2facdb1c86b6e52a140881f607a
AGT-78 <AGT-78@users.noreply.github.com>
2025-02-05 04:51:30 +0000

    deploy: d10de566cdea7948db2e33afaf3238ce51efb7b7

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "to-do-pwa-cache-v1";
+const FILES_TO_CACHE = [
+  "/To-Do-List/",
+  "/To-Do-List/index.html",
+  "/To-Do-List/style.css",
+  "/To-Do-List/app.js",
+  "/To-Do-List/manifest.json",
+  "/To-Do-List/icons/icon-128.png",
+  "/To-Do-List/icons/icon-512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
