46d89bc98bc0b694a9b61199c3351ac5e47e8fa4
AGT-78 <AGT-78@users.noreply.github.com>
2025-02-07 05:57:31 +0000

    deploy: 761ed9a93835ac923aa18e7a7f1acc5cac89a692

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const sw = new URL("service-worker.js", import.meta.url);
 if ("serviceWorker" in navigator) {
   const s = navigator.serviceWorker;
   s.register(sw.href, {
-    scope: "/To-Do-List/",
+    scope: "/CheckList/",
   })
     .then((_) =>
       console.log(
@@ -26,13 +26,12 @@ if ("serviceWorker" in navigator) {
 }
 
 const firebaseConfig = {
-  apiKey: "AIzaSyASGJwSz4tznFWBampVkL2F9ygpz9bNz6w",
-  authDomain: "to-do-list-c5fe5.firebaseapp.com",
-  projectId: "to-do-list-c5fe5",
-  storageBucket: "to-do-list-c5fe5.firebasestorage.app",
-  messagingSenderId: "604975788039",
-  appId: "1:604975788039:web:bc5a4319b01ddc01a43aa2",
-  measurementId: "G-B3GL8GGZG4",
+  apiKey: "AIzaSyCuWLYWUJ2R4v6ptAKcL2jmXJPDSGl7uW0",
+  authDomain: "info5146.firebaseapp.com",
+  projectId: "info5146",
+  storageBucket: "info5146.firebasestorage.app",
+  messagingSenderId: "711911517967",
+  appId: "1:711911517967:web:cadd485436fb294a85987a",
 };
 
 const app = initializeApp(firebaseConfig);
@@ -50,15 +49,12 @@ window.addEventListener("load", () => {
 addTaskBtn.addEventListener("click", async () => {
   const task = taskInput.value.trim();
   if (task) {
-    const taskInput = document.getElementById("taskInput");
-    const taskText = sanitizeInput(taskInput.value.trim());
+    let taskId = await addTaskToFirestore(task);
+    taskInput.value = "";
 
-    if (taskText) {
-      await addTaskToFirestore(taskText);
-      renderTasks();
-      taskInput.value = "";
-    }
-    renderTasks();
+    createLiTask(taskId, task);
+  } else {
+    alert("Please enter a task!");
   }
 });
 
@@ -68,56 +64,72 @@ taskList.addEventListener("click", async (e) => {
     await updateDoc(doc(db, "todos", e.target.id), {
       completed: true,
     });
+    e.target.remove();
   }
-  renderTasks();
 });
 
 async function renderTasks() {
   var tasks = await getTasksFromFirestore();
   taskList.innerHTML = "";
 
-  tasks.forEach((task, index) => {
-    if (!task.data().completed) {
-      const taskItem = document.createElement("li");
-      taskItem.id = task.id;
-      taskItem.textContent = task.data().text;
-      taskList.appendChild(taskItem);
+  let taskArr = [];
+
+  tasks.forEach((task) => {
+    taskArr.push({
+      id: task.id,
+      text: task.data().text,
+      completed: task.data().completed,
+    });
+  });
+
+  taskArr.sort(function (a, b) {
+    return new Date(b.timeCreated) - new Date(a.timeCreated);
+  });
+
+  taskArr.forEach((task) => {
+    if (!task.completed) {
+      createLiTask(task.id, task.text);
     }
   });
 }
 
 async function addTaskToFirestore(taskText) {
-  await addDoc(collection(db, "todos"), {
+  let task = await addDoc(collection(db, "todos"), {
     text: taskText,
     completed: false,
   });
+  return task.id;
 }
 
 async function getTasksFromFirestore() {
-  var data = await getDocs(collection(db, "todos"));
-  let userData = [];
-  data.forEach((doc) => {
-    userData.push(doc);
-  });
-  return userData;
+  return await getDocs(collection(db, "todos"));
 }
 
-function sanitizeInput(input) {
-  const div = document.createElement("div");
-  div.textContent = input;
-  return div.innerHTML;
+function createLiTask(id, text) {
+  let taskItem = document.createElement("li");
+  taskItem.id = id;
+  taskItem.textContent = text;
+  taskItem.tabIndex = 0;
+  taskList.appendChild(taskItem);
 }
+
+//Allow task addition on enter key while in task input
+taskInput.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    addTaskBtn.click();
+  }
+});
+
+//Allow tasks to be completed on enter
+taskList.addEventListener("keypress", async function (e) {
+  if (e.target.tagName === "LI" && e.key === "Enter") {
+    await updateDoc(doc(db, "todos", e.target.id), {
+      completed: true,
+    });
+  }
+  renderTasks();
+});
 
 window.addEventListener("error", function (event) {
   console.error("Error occurred: ", event.message);
 });
-
-import log from "loglevel";
-
-// Set the log level (trace, debug, info, warn, error)
-log.setLevel("info");
-
-// Example logs
-log.info("Application started");
-log.debug("Debugging information");
-log.error("An error occurred");
